2fb794e1d976ea8f1b13c27094b01a7337ea987c
Reem Mohamed <ream2004523farouk@gmail.com>
2025-09-11 00:57:08 +0300

    Add node-addon-api conversion script and node-gyp-build module
    - Introduced a new script for converting node-addon-api code to Napi.
    - Added LICENSE, README.md, SECURITY.md, and bin.js files for node-gyp-build.
    - Implemented build-test.js and index.js for handling native module builds.
    - Included optional.js for managing optional native dependencies.
    - Updated package.json for node-gyp-build with necessary metadata and scripts.

--- Day 15/index.js
+++ Day 15/index.js
@@ -5,15 +5,11 @@ const {connectDB}=require('./config/connectDb');
 const { mongoose } = require('mongoose');
 const session = require('express-session');
 app.use(express.json());
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.static(path.join(__dirname,'public')));
 app.set('json spaces',2)
 connectDB();
 
-
 app.use(session({
-    secret:"kfkfkfkfkkfkfkw;,w;,;,s;,s;w,s;w",
+    secret:"mysercetKey",
     saveUninitialized:false,
     resave:false,
     cookie:{
@@ -21,20 +17,7 @@ app.use(session({
         httpOnly:true,
         secure:false
     }
-}))
-app.post('/login',(req,res)=>{
-    let {name,password}=req.body
-    req.session.user={name,password}
-    console.log(req.session);
-    console.log(req.sessionID);
-    return res.status(200).send("user logined successfully !!")
-})
-app.get('/',(req,res)=>{
-    // res.send("Home Page from index.js")
-    console.log(req.session);
-    if(req.session.user)
-        return res.status(200).json(req.session.user)
-});
+}));
 
 mongoose.connection.once('open',()=>{
     console.log("MongoDB connected");
